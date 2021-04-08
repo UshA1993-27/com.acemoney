ce659d8ff5047788698b5ebd54d758a87649f7b5
UshA1993-27 <usha081993@gmail.com>
2021-04-08 01:53:16 -0700

    first commit

---
+++ src/test/java/com/acemoney/basics.java
@@ -0,0 +1,25 @@
+package com.acemoney;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class basics {
+	@BeforeTest
+	public void setUp()
+	{
+		
+		
+	}
+@Test
+public void verifyTitle()
+{
+	
+	
+}
+@AfterTest
+public void teardown()
+{
+	
+}
+}
